3d995c45345b533f3b42d68e1b286ea2e9934da8
Nico <nmanograsso@gmail.com>
2023-03-06 10:43:39 -0300

    rename: to SwiftNetworking

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Networking",
+    name: "SwiftNetworking",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Networking",
-            targets: ["Networking"]),
+            name: "SwiftNetworking",
+            targets: ["SwiftNetworking"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Networking",
+            name: "SwiftNetworking",
             dependencies: []),
         .testTarget(
-            name: "NetworkingTests",
-            dependencies: ["Networking"]),
+            name: "SwiftNetworkingTests",
+            dependencies: ["SwiftNetworking"]),
     ]
 )
